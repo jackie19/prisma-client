f46f5819d8a10a5e15e36765848894e7269b0454
jackie19 <jackie198512@126.com>
2024-12-04 03:46:20 +0800

    2.85.3: init driverAdapters

--- index.d.ts
+++ index.d.ts
@@ -3930,6 +3930,10 @@ export namespace Prisma {
       timeout?: number
       isolationLevel?: Prisma.TransactionIsolationLevel
     }
+    /**
+     * Instance of a Driver Adapter, e.g., like one provided by `@prisma/adapter-planetscale`
+     */
+    adapter?: runtime.DriverAdapter | null
   }
 
 
